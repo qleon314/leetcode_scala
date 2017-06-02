88c221c56489b2937a155e8b9c4f1d87adc43a79
lqin200 <lqin@g.clemson.edu>
2017-06-02 13:27:10 -0400

    reformat project

--- src/main/scala/com/melo/scala/solutions/core/jobs.scala
+++ src/main/scala/com/melo/scala/solutions/core/jobs.scala
@@ -1,10 +1,10 @@
 package com.melo.scala.solutions.core
 
 /**
-  * Created by lqin200 on 5/14/17.
+  * Created by lqin200 on 6/2/17.
   */
-trait jobs {
-  def main(args: Array[String]): Unit = {
+trait Jobs {
+  def runJob(): Unit = {
 
   }
 }
